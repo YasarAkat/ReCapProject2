d32073cc60561163e462e80df328677a770f4f22
Yaşar Mehmet Akat <akat434@gmail.com>
2023-07-19 19:08:42 +0300

    Add project files.

---
+++ Business/Concrete/CarManager.cs
@@ -0,0 +1,77 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class CarManager : ICarService
+    {
+        ICarDal _carDal;
+
+        public CarManager(ICarDal carDal)
+        {
+            _carDal = carDal;
+        }
+        public void Add(Car car)
+        {
+            if (car.CarName.Length < 2)
+            {
+                Console.WriteLine("The Car Name must have minimum 2 character.");
+            }
+            else if (car.DailyPrice <= 0)
+            {
+                Console.WriteLine("Daily Price must be greater than zero.");
+            }
+            else
+            {
+                _carDal.Add(car);
+            }
+        }
+
+        public void Delete(Car car)
+        {
+            _carDal.Delete(car);
+        }
+
+        public List<Car> GetAll()
+        {
+            return _carDal.GetAll();
+        }
+
+        public Car GetById(int carId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Car> GetCarsByBrandId(int BrandId)
+        {
+            return _carDal.GetAll(p => p.BrandId == BrandId);
+        }
+
+        public List<Car> GetCarsByColorId(int ColorId)
+        {
+            return _carDal.GetAll(p => p.ColorId == ColorId);
+        }
+
+        public void Update(Car car)
+        {
+            if (car.CarName.Length < 2)
+            {
+                Console.WriteLine("The Car Name must have minimum 2 character.");
+            }
+            else if (car.DailyPrice <= 0)
+            {
+                Console.WriteLine("Daily Price must be greater than zero.");
+            }
+            else
+            {
+                _carDal.Add(car);
+            }
+        }
+    }
+}
---
+++ ConsolUI/Program.cs
@@ -0,0 +1,35 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+
+namespace ConsolUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            CarManager carManager = new CarManager(new EfCarDal());
+
+            //foreach (var car in carManager.GetAll())
+            //{
+            //    Console.WriteLine($"Model: {car.CarName} Marka: {car.BrandId} Renk: {car.ColorId} Günlük Fiyat: {car.DailyPrice} Açıklama: {car.Description}");
+            //}
+
+            //carManager.Add(new Car { CarId = 6, BrandId = 1, ColorId = 2, CarName = "32A", DailyPrice = 0, ModelYear = "2021", Description = "Çok pahalı araba" });
+            //carManager.Add(new Car { CarId = 2, BrandId = 4, ColorId = 3, CarName = "Astra", DailyPrice = 2000, ModelYear = "2020", Description = "pahalı araba" });
+            //carManager.Add(new Car { CarId = 3, BrandId = 3, ColorId = 2, CarName = "320D", DailyPrice = 1500, ModelYear = "2021", Description = "Konforlu araba" });
+            //carManager.Add(new Car { CarId = 4, BrandId = 2, ColorId = 4, CarName = "A5 Coupe", DailyPrice = 1000, ModelYear = "2020", Description = "Uygun araba" });
+            //carManager.Add(new Car { CarId = 5, BrandId = 1, ColorId = 1, CarName = "S500", DailyPrice = 1500, ModelYear = "2021", Description = "Performanslı araba" });
+            carManager.Delete(new Car { CarId = 2, BrandId = 4, ColorId = 3, CarName = "Astra", DailyPrice = 2000, ModelYear = "2020", Description = "pahalı araba" });
+
+
+
+
+        }
+
+       
+    }
+}
